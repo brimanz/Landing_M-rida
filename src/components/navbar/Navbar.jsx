ed5d44bd5d57ec6389bbbdd87fa7d301650a27c1
brimanz <brimanz2014@gmail.com>
2022-07-24 17:32:38 -0400

    finishing the navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BsFillGrid3X3GapFill, BsFillXCircleFill} from 'react-icons/bs';
 import './Navbar.css';
 import Logo from '../../assets/logo.jpg'
 
 
-const Navbar = () =>{
+const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+
   return(
     <nav className="app__navbar">
 
@@ -28,37 +32,37 @@ const Navbar = () =>{
       </ul>
 
       <div className="app__navbar-smallscreen">
-        <BsFillGrid3X3GapFill 
+        <BsFillGrid3X3GapFill
           color="#c31432" 
           fontSize={25}
-          onClick={() => {}}
+          onClick={() => setToggleMenu(true)}
+          cursor="pointer"
         />
 
-        <div>
-          <BsFillXCircleFill
-            color="#c31432" 
-            fontSize={25}
-            onClick={() => {}}
-          />
+        {toggleMenu && (
+            <div className="app__navbar-smallscreen_overlay">
+              <BsFillXCircleFill
+                className="overlay__close"
+                onClick={() => setToggleMenu(false)}
+              />
 
-          <ul className="app__navbar-smallscreen-links">
-            <li>
-              <a href="#home">Inicio</a>
-            </li>
-            <li>
-              <a href="#about">Sobre Mérida</a>
-            </li>
-            <li>
-              <a href="#places">Lugares</a>
-            </li>
-            <li>
-              <a href="#activity">Actividades</a>
-            </li>
-          </ul>
-
-        </div>
+              <ul className="app__navbar-smallscreen_links">
+                <li>
+                  <a href="#home">Inicio</a>
+                </li>
+                <li>
+                  <a href="#about">Sobre Mérida</a>
+                </li>
+                <li>
+                  <a href="#places">Lugares</a>
+                </li>
+                <li>
+                  <a href="#activity">Actividades</a>
+                </li>
+              </ul>
+          </div>
+        )}
       </div>
-
     </nav>
   );
 }
