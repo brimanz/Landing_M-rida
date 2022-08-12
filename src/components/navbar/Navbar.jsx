626c7641cf15c867db703230800247e0d5490f61
jonathan_briceño <brimanz2014@gmail.com>
2022-08-12 12:27:46 -0400

    fixing navbar styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {BsFillGrid3X3GapFill, BsFillXCircleFill} from 'react-icons/bs';
 import './Navbar.css';
-import Logo from '../../assets/logo.jpg'
+import Logo from '../../assets/logo.png'
 
 
 const Navbar = () => {
@@ -34,7 +34,7 @@ const Navbar = () => {
       <div className="app__navbar-smallscreen">
         <BsFillGrid3X3GapFill
           color="#c31432" 
-          fontSize={25}
+          fontSize={24}
           onClick={() => setToggleMenu(true)}
           cursor="pointer"
         />
