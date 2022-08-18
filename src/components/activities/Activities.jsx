9f6c2c2b2a580a0c2d4d12668524c88cd4bd5084
jonathan_briceño <brimanz2014@gmail.com>
2022-08-18 19:39:25 -0400

    activities component and some styles

--- src/components/activities/Activities.jsx
+++ src/components/activities/Activities.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
 import './Activities.css'
+import {GrBike, GrRun, GrYoga, GrSwim} from "react-icons/gr";
+
+
 
 
 const Activities = () =>{
 	return(
 		<div className="app__activities" id="activities">
 			<div className="app__activities_content">
+				<GrRun className="app__activities_icon"/>
 				<h1>Caminata</h1>
 				<p>Los mejores lugares para conocer y disfrutar</p>
 			</div>
 
 			<div className="app__activities_content">
+				<GrBike className="app__activities_icon"/>
 				<h1>Ciclismo</h1>
 				<p>Rutas hermosas acampañadas de la naturalesa</p>
 			</div>
 
 			<div className="app__activities_content">
-				<h1>Canotaje</h1>
-				<p>Para disfrutar de la fuerza y el poder de los rios de Mérida</p>
+				<GrSwim className="app__activities_icon"/>
+				<h1>Natación</h1>
+				<p>Disfruta la belleza de los rios de Mérida</p>
 			</div>
 
 			<div className="app__activities_content">
-				<h1>Paracaidismo y parapente</h1>
-				<p>Para disfrutar de los cielos de Mérida y sentir la adrenalida en todo el cuerpo</p>
+				<GrYoga className="app__activities_icon"/>
+				<h1>Yoga</h1>
+				<p>Consigue la armonia y relajate</p>
 			</div>
 		</div>
 	);
