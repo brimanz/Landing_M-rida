e8d4871cbffb15f09b392199931c61324d729156
jonathan_briceño <brimanz2014@gmail.com>
2022-08-21 20:41:36 -0400

    app update and places component begin

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import './index.css';
 
 //components import
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
 import Slider from './components/slider/Slider';
 import Activities from './components/activities/Activities';
+import Places from './components/places/Places';
 
-//styles
-import './index.css';
 
 //images
 import Bg from './assets/bg.jpg';
 import Img2 from "./assets/paisaje.jpg"
 import Img3 from "./assets/cultura.jpg"
 import Img4 from "./assets/pueblo.jpg"
+import Img5 from "./assets/laguna.jpg"
+import Img6 from "./assets/merida.jpg"
 
 
 //navbar elements
@@ -32,6 +34,7 @@ const App = () =>{
       <Navbar navbarLinks={navbarLinks}/>
       <Hero imageSrc={Bg}/>
 
+      {/*about merida*/}
       <Slider 
         imageSrc={Img2} 
         title={"Paisajes hermosos"}
@@ -51,6 +54,20 @@ const App = () =>{
       />
 
       <Activities/>
+
+      {/*merida places*/}
+      <Places 
+        imageSrc={Img5} 
+        title={"Lagunas y aguas termales"}
+        subtitle={"Lugares hermosos y llenos de romance"}
+        flipped={true}
+      />
+      <Places 
+        imageSrc={Img6} 
+        title={"Páramos lugares de altura"}
+        subtitle={"Tierra donde los trabajadores cultivan"}
+        flipped={false}
+      />
 
     </div>
   );
